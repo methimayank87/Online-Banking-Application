dd8c5bfc8182dbb7864b70b322fb93e96bd4e3c2
Mayank Methi <31508261+methimayank87@users.noreply.github.com>
2020-09-19 09:26:27 +0530

    Added Demo Controller

---
+++ backend/OnlineBankingApplication/OnlineBankingApplication/Controllers/DemoController.cs
@@ -0,0 +1,17 @@
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace OnlineBankingApplication.Controllers
+{
+    public class DemoController : Controller
+    {
+        // GET: Demo
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
