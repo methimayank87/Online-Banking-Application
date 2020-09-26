9077fc67da0d7d802330fd70fea8f78f372b3040
kanwaljeet-kaur <70679259+kanwaljeet-kaur@users.noreply.github.com>
2020-09-26 11:30:58 +0530

    Authorization

---
+++ Backend/OnlineBankingApplication/OnlineBankingApplication/Models/AuthRepository.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace OnlineBankingApplication.Models
+{
+    public class AuthRepository
+    {
+        public bool ValidateUser(string Username, string Password)
+        {
+
+            return Username == "bhushan" && Password == "demo";
+        }
+    }
+}
