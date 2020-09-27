c6e16046e5ea0b69c27d2f111653e7a1db0ac1f7
nimrodcoder <gupta.niharika107@gmail.com>
2020-09-28 00:23:38 +0530

    added account creation logic

--- AngularFrontend/src/app/components/userlogin/userlogin.component.ts
+++ AngularFrontend/src/app/components/userlogin/userlogin.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import {FormBuilder, FormControl, FormGroup, Validators} from"@angular/forms";
 import {Router} from"@angular/router";
+import { UserService } from 'src/app/services/user.service';
+import { User } from 'src/app/model/User';
 
 @Component({
   selector: 'app-userlogin',
@@ -10,8 +12,8 @@ import {Router} from"@angular/router";
 export class UserloginComponent implements OnInit {
 
   userloginForm:FormGroup;
-
-  constructor(private formBuilder: FormBuilder, private router: Router) {
+  currentUser: User;
+  constructor(private formBuilder: FormBuilder, private router: Router, private userService: UserService) {
 
     this.userloginForm = this.formBuilder.group({
       userid:new FormControl('', Validators.required),
@@ -25,7 +27,25 @@ export class UserloginComponent implements OnInit {
 
   onSubmit(form)
   {
-    alert("Successfully logged in!");
+    // this.userService.getUserById(form.value.userid).subscribe(data => {
+    //   try{
+    //     this.currentUser = data;
+    //     this.userService.loginUser(this.currentUser, form.value.password).subscribe(data => {
+    //       try{
+    //         alert("Successfully logged in!");
+    //       }catch(err){
+    //         console.log(err)
+    //         alert("Wrong Password")
+    //       }
+         
+    //     })
+    //   } catch(err){
+    //     console.log(err)
+    //         alert("User not Found")
+    //   }
+      
+    // })
+    
     this.router.navigate(['dashboard']);
   }
 
