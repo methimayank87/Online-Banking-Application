715e73a54ea33fe9560228d4025c43627db916cf
Shibangi111 <70949719+Shibangi111@users.noreply.github.com>
2020-09-30 14:10:06 +0530

    Added

---
+++ AngularFrontend/src/app/components/dashboardnavbar/dashboardnavbar.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-dashboardnavbar',
+  templateUrl: './dashboardnavbar.component.html',
+  styleUrls: ['./dashboardnavbar.component.css']
+})
+export class DashboardnavbarComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
